6313d1389fef697178b6ecc41f23e38238ca529d
Jusley Souza <jusleysouza31@gmail.com>
2024-07-18 19:38:26 -0300

    Main Class

---
+++ src/main/java/br/com/ju/webflux/course/WebfluxCourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.ju.webflux.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxCourseApplication.class, args);
+	}
+
+}
