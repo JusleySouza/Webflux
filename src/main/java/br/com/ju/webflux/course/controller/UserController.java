0cf56aa48bff2cd955bdbb210ef88d59ab8397e4
Jusley Souza <jusleysouza31@gmail.com>
2024-07-20 10:26:44 -0300

    Adding @Component annotation

--- src/main/java/br/com/ju/webflux/course/controller/UserController.java
+++ src/main/java/br/com/ju/webflux/course/controller/UserController.java
@@ -1,6 +1,7 @@
 package br.com.ju.webflux.course.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,6 +14,7 @@ import br.com.ju.webflux.course.model.response.UserResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Component
 public interface UserController {
 	
 	@PostMapping
