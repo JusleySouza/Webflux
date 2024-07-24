adaccb1a63de62d9c85b9eba609c8a177597594d
Jusley Souza <jusleysouza31@gmail.com>
2024-07-23 21:45:27 -0300

    Adding ObjectNotFoundException method

--- src/main/java/br/com/ju/webflux/course/controller/exceptions/ControllerExceptionsHandler.java
+++ src/main/java/br/com/ju/webflux/course/controller/exceptions/ControllerExceptionsHandler.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
+
+import br.com.ju.webflux.course.service.exception.ObjectNotFoundException;
+
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.validation.FieldError;
 
@@ -12,6 +15,7 @@ import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class ControllerExceptionsHandler {
@@ -44,6 +48,22 @@ public class ControllerExceptionsHandler {
 		}
 		
 		return ResponseEntity.status(BAD_REQUEST).body(Mono.just(error));
+	}
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	ResponseEntity<Mono<StandardError>> objectNotFoundException(
+			ObjectNotFoundException ex, ServerHttpRequest request
+			){
+		return ResponseEntity.status(NOT_FOUND)
+				.body(Mono.just(
+						StandardError.builder()
+						.timestamp(now())
+						.status(NOT_FOUND.value())
+						.error(NOT_FOUND.getReasonPhrase())
+						.message(ex.getMessage())
+						.path(request.getPath().toString())
+						.build()
+						));
 	}
 	
 	private String verifyDupKey(String message) {
