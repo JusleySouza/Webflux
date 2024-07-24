c5442f80f05a25d34923c717ffe007d2b9b87632
Jusley Souza <jusleysouza31@gmail.com>
2024-07-24 15:55:06 -0300

    Implementation of the update route

--- src/main/java/br/com/ju/webflux/course/controller/impl/UserControllerImpl.java
+++ src/main/java/br/com/ju/webflux/course/controller/impl/UserControllerImpl.java
@@ -41,8 +41,8 @@ public class UserControllerImpl implements UserController{
 
 	@Override
 	public ResponseEntity<Mono<UserResponse>> update(String id, UserRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		return ResponseEntity.ok().body(
+				service.update(id, request).map(mapper::toResponse));
 	}
 
 	@Override
