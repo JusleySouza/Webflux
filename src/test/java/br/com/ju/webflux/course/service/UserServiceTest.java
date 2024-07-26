58e9e42fb73c2ab7438e6bd567125c30d32d36b6
Jusley Souza <jusleysouza31@gmail.com>
2024-07-26 18:08:37 -0300

    Creation and implementation of the findById test

--- src/test/java/br/com/ju/webflux/course/service/UserServiceTest.java
+++ src/test/java/br/com/ju/webflux/course/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.ju.webflux.course.service;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 import java.util.Objects;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.ju.webflux.course.entity.User;
@@ -30,7 +33,7 @@ class UserServiceTest {
 	private UserService service;
 
 	@Test
-	void save() {
+	void testSave() {
 		UserRequest request = new UserRequest("Sara Mello", "sara@mail.com", "123");
 		User entity = User.builder().build();
 		
@@ -38,10 +41,27 @@ class UserServiceTest {
 		when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
 		
 		Mono<User> result = service.save(request);
+		
 		StepVerifier.create(result)
 		.expectNextMatches(Objects::nonNull)
 		.expectComplete()
 		.verify();
+		
+		Mockito.verify(repository, times(1)).save(any(User.class));
+	}
+	
+	@Test
+	void testFindById() {
+		when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().build()));
+		
+		Mono<User> result = service.findById("123");
+		
+		StepVerifier.create(result)
+		.expectNextMatches(user -> user.getClass() == User.class)
+		.expectComplete()
+		.verify();
+		
+		Mockito.verify(repository, times(1)).findById(anyString());
 	}
 
 }
