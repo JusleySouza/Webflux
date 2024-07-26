6a5dd66cc0fa8e314e8fae5a8ba7de6eb57f362e
Jusley Souza <jusleysouza31@gmail.com>
2024-07-26 18:32:57 -0300

    Creation and implementation of the Delete service test

--- src/test/java/br/com/ju/webflux/course/service/UserServiceTest.java
+++ src/test/java/br/com/ju/webflux/course/service/UserServiceTest.java
@@ -97,5 +97,20 @@ class UserServiceTest {
 		
 		Mockito.verify(repository, times(1)).save(any(User.class));
 	}
+	
+	@Test
+	void testDelete() {
+		User entity = User.builder().build();
+		when(repository.findAndRemove(anyString())).thenReturn(Mono.just(entity));
+		
+		Mono<User> result = service.delete("123");
+		
+		StepVerifier.create(result)
+		.expectNextMatches(Objects::nonNull)
+		.expectComplete()
+		.verify();
+		
+		Mockito.verify(repository, times(1)).findAndRemove(anyString());
+	}
 
 }
