19d77512e78d49ccc0fb3df2f4bbef7269211a4f
Jusley Souza <jusleysouza31@gmail.com>
2024-07-26 18:53:35 -0300

    Removing unused imports

--- src/test/java/br/com/ju/webflux/course/service/UserServiceTest.java
+++ src/test/java/br/com/ju/webflux/course/service/UserServiceTest.java
@@ -9,7 +9,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.Objects;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
