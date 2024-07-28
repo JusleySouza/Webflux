1e76ace2175897ad6b19176739382e186241069e
Jusley Souza <jusleysouza31@gmail.com>
2024-07-27 22:10:54 -0300

    Creation and implementation the integration test for the saveWithBadRequestEmptyName endpoint

--- src/test/java/br/com/ju/webflux/course/controller/UserControllerImplTest.java
+++ src/test/java/br/com/ju/webflux/course/controller/UserControllerImplTest.java
@@ -57,8 +57,8 @@ class UserControllerImplTest {
 	}
 	
 	@Test
-	@DisplayName("Test endpoint save with bad request")
-	void testSaveWithBadRequest() {
+	@DisplayName("Test endpoint save with bad request for name with spaces at the beginning")
+	void testSaveWithBadRequestForNameWithSpaces() {
 		final var request = new UserRequest(" Sara Mello", "sara@mail.com", "123");
 			
 		webTestClient.post().uri("/users")
@@ -73,6 +73,25 @@ class UserControllerImplTest {
 		.jsonPath("$.message").isEqualTo("Error on validation attributes")
 		.jsonPath("$.errors[0].fieldName").isEqualTo("name")
 		.jsonPath("$.errors[0].message").isEqualTo("Field cannot have blank spaces at the beginning or at and");
+	}
+	
+	@Test
+	@DisplayName("Test endpoint save with bad request for empty name")
+	void testSaveWithBadRequestEmptyName() {
+		final var request = new UserRequest(null , "sara@mail.com", "123");
+			
+		webTestClient.post().uri("/users")
+		.contentType(APPLICATION_JSON)
+		.body(fromValue(request))
+		.exchange()
+		.expectStatus().isBadRequest()
+		.expectBody()
+		.jsonPath("$.path").isEqualTo("/users")
+		.jsonPath("$.status").isEqualTo(BAD_REQUEST.value())
+		.jsonPath("$.error").isEqualTo("Validation Error")
+		.jsonPath("$.message").isEqualTo("Error on validation attributes")
+		.jsonPath("$.errors[0].fieldName").isEqualTo("name")
+		.jsonPath("$.errors[0].message").isEqualTo("must not be null or empty");
 	}
 	
 	@Test
